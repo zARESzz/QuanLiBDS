b08505262417fbb8f1fea1388faf9e08d67ecba4
TuanAnh2401 <lta24012k2@gmail.com>
2022-09-23 22:53:56 +0700

    Add Data

---
+++ Main/cls_ChucVu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+namespace Main
+{
+    public class cls_ChucVu
+    {
+        data_BDSEntities db = new data_BDSEntities();
+        public List<CHUCVU> getList()
+        {
+            return db.CHUCVUs.ToList();
+        }
+    }
+}
--- QuanLy/QuanLy.cs
+++ QuanLy/QuanLy.cs
@@ -1,15 +1,13 @@
-﻿using DevExpress.XtraBars;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuanLiBDS
+namespace QuanLy
 {
     public partial class QuanLy : DevExpress.XtraBars.Ribbon.RibbonForm
     {
